07c2a63299e36219f95112401923a7768cb5ce4e
susanagomezserna <143086959+susanagomezserna@users.noreply.github.com>
2023-08-24 07:09:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+from PIL import Image
 
 st.title("Mi primera aplicación")
 st.write("by: Susana Gómez Serna")
+
+st.Header("En este espacio comienzo a desarrollar mis aplicaciones")
+st.write("Esta es una imagen de mi perro")
+image = Image.open('IMG_4030.JPG')
+
+st.image(image, caption = ´Jager´)
