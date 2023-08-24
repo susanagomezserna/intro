67cfd5ee0b5d5a5ae4e4a8ca56da1e80242e1ad9
susanagomezserna <143086959+susanagomezserna@users.noreply.github.com>
2023-08-24 07:18:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,10 @@ st.write("Esta es una imagen de mi perro")
 image = Image.open('IMG_4030.JPG')
 
 st.image(image, caption = 'Jager')
+
+texto = st.text_input('Escribe algo','Este es mi texto')
+st.write('El texto escrito es:', texto)
+
+st.subheader("Ahora usemos 2 columnas")
+col1.col2 = st.columns(2)
+
